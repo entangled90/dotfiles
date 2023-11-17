7527e69c3073e6d8318058e7239b3636086e7c06
entangled90 <sana.carlo@gmail.com>
2023-11-17 19:42:17 +0100

    added nvim-metals config

--- nvim/init.lua
+++ nvim/init.lua
@@ -73,8 +73,6 @@ require('lazy').setup({
   'tpope/vim-fugitive',
   'tpope/vim-rhubarb',
 
-  {'scalameta/nvim-metals', dependencies = { "nvim-lua/plenary.nvim" }},
-
    "williamboman/mason.nvim",
    "mfussenegger/nvim-dap",
    "jay-babu/mason-nvim-dap.nvim",
@@ -82,7 +80,7 @@ require('lazy').setup({
   "vim-test/vim-test",
   -- Detect tabstop and shiftwidth automatically
   'tpope/vim-sleuth',
-
+  {'scalameta/nvim-metals', dependencies = { "nvim-lua/plenary.nvim" }},
   -- NOTE: This is where your plugins related to LSP can be installed.
   --  The configuration is done below. Search for lspconfig to find it below.
   {
@@ -101,6 +99,7 @@ require('lazy').setup({
       'folke/neodev.nvim',
     },
   },
+
 
   {
     -- Autocompletion
@@ -494,6 +493,76 @@ mason_lspconfig.setup_handlers {
     }
   end
 }
+
+--- CONFIGURATION FOR METALS
+----------------------------------
+-- LSP Setup ---------------------
+----------------------------------
+
+----------------------------------
+-- OPTIONS -----------------------
+----------------------------------
+-- global
+vim.opt_global.completeopt = { "menuone", "noinsert", "noselect" }
+
+local api = vim.api
+local cmd = vim.cmd
+local map = vim.keymap.set
+-- LSP mappings
+map("n", "gD",  vim.lsp.buf.definition)
+map("n", "K",  vim.lsp.buf.hover)
+map("n", "gi", vim.lsp.buf.implementation)
+map("n", "gr", vim.lsp.buf.references)
+map("n", "gds", vim.lsp.buf.document_symbol)
+map("n", "gws", vim.lsp.buf.workspace_symbol)
+map("n", "<leader>cl", vim.lsp.codelens.run)
+map("n", "<leader>sh", vim.lsp.buf.signature_help)
+map("n", "<leader>rn", vim.lsp.buf.rename)
+map("n", "<leader>f", vim.lsp.buf.format)
+map("n", "<leader>ca", vim.lsp.buf.code_action)
+
+map("n", "<leader>ws", function()
+  require("metals").hover_worksheet()
+end)
+
+-- all workspace diagnostics
+map("n", "<leader>aa", vim.diagnostic.setqflist)
+
+-- all workspace errors
+map("n", "<leader>ae", function()
+  vim.diagnostic.setqflist({ severity = "E" })
+end)
+
+local metals_config = require("metals").bare_config()
+
+-- Example of settings
+metals_config.settings = {
+  showImplicitArguments = true,
+  excludedPackages = { "akka.actor.typed.javadsl", "com.github.swagger.akka.javadsl" },
+}
+
+-- Autocmd that will actually be in charging of starting the whole thing
+local nvim_metals_group = api.nvim_create_augroup("nvim-metals", { clear = true })
+api.nvim_create_autocmd("FileType", {
+  -- NOTE: You may or may not want java included here. You will need it if you
+  -- want basic Java support but it may also conflict if you are using
+  -- something like nvim-jdtls which also works on a java filetype autocmd.
+  pattern = { "scala", "sbt", "java" },
+  callback = function()
+    require("metals").initialize_or_attach(metals_config)
+  end,
+  group = nvim_metals_group,
+})
+
+-- *READ THIS*
+-- I *highly* recommend setting statusBarProvider to true, however if you do,
+-- you *have* to have a setting to display this in your statusline or else
+-- you'll not see any messages from metals. There is more info in the help
+-- docs about this
+-- metals_config.init_options.statusBarProvider = "on"
+
+-- Example if you are using cmp how to make sure the correct capabilities for snippets are set
+metals_config.capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 -- [[ Configure nvim-cmp ]]
 -- See `:help cmp`
