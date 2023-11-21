7b104479f542209581ff31b320f91e32c131215e
entangled90 <sana.carlo@gmail.com>
2023-11-21 21:29:13 +0100

    added alacritty configs for nerd fonts

--- nvim/init.lua
+++ nvim/init.lua
@@ -167,9 +167,14 @@ require('lazy').setup({
     'lukas-reineke/indent-blankline.nvim',
     -- Enable `lukas-reineke/indent-blankline.nvim`
     -- See `:help indent_blankline.txt`
+    main = "ibl",
     opts = {
-      char = '┊',
-      show_trailing_blankline_indent = false,
+      indent = {
+        char = '┊',
+      },
+      scope = {
+        show_exact_scope = true,
+      },
     },
   },
 
