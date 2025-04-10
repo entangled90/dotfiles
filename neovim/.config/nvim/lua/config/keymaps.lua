bc3ce53a59d89c91fc8917ec52d5a22b42672591
Carlo Sana <sana.carlo@gmail.com>
2025-04-10 10:28:47 +0200

    add remap jj -> <Esc>

--- neovim/.config/nvim/lua/config/keymaps.lua
+++ neovim/.config/nvim/lua/config/keymaps.lua
@@ -5,3 +5,7 @@
 --add them back when java is working
 -- map("n", "<leader>tc", require("jdtls").test_class, { desc = "test current class" })
 -- map("n", "<leader>tm", require("jdtls").test_nearest_method, { desc = "test method" })
+--
+local map = vim.keymap.set
+
+map("i", "jj", "<Esc>")
